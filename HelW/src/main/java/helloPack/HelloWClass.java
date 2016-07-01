1b7aa35b36c2eacb91a879db8805b78be64a6b6a
dan <gh>
2016-07-01 10:47:19 +0300

    Updated all srcs

---
+++ HelW/src/main/java/helloPack/HelloWClass.java
@@ -0,0 +1,11 @@
+package helloPack;
+
+public class HelloWClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hw");
+		
+	}
+
+}
---
+++ HelW/src/test/java/testPack/tstCls.java
@@ -0,0 +1,10 @@
+package testPack;
+
+public class tstCls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
